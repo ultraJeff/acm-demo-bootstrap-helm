529e37cb1863097aec94e22fa7dbd2c4c3c98770
Jeff Franklin <jefr@redhat.com>
2025-03-21 16:03:46 -0400

    Fix the update-values.sh script

--- update-values.sh
+++ update-values.sh
@@ -11,6 +11,7 @@ read_value() {
     local key=$1
     local value=""
     local line
+    local first=true
     local reading=false
     
     while IFS= read -r line; do
@@ -19,41 +20,51 @@ read_value() {
             if [[ $line =~ ^[A-Z_]+=.*$ ]]; then
                 break
             fi
-            # Append the line
-            value+="$line\n"
+            # Add newline before appending if not the first line
+            if [[ $first == false ]]; then
+                value+=$'\n'
+            fi
+            value+="$line"
+            first=false
         elif [[ $line == "$key="* ]]; then
             # Start reading from the first line after the key
             reading=true
-            value+="${line#*=}\n"
+            value+="${line#*=}"
+            first=false
         fi
     done
     
-    # Remove trailing newline
-    value=${value%$'\n'}
     echo "$value"
 }
 
 # Process each key
-while IFS='=' read -r line; do
+while IFS='=' read -r line || [ -n "$line" ]; do
     # Skip empty lines and comments
-    [[ -z "$line" || "$line" =~ ^# ]] && continue
+    [[ -z "$line" || "$line" =~ ^[[:space:]]*# ]] && continue
     
     # Get the key
     key=${line%%=*}
+    key=${key%%[[:space:]]}  # Remove trailing spaces
     [[ -z "$key" ]] && continue
     
     # Read the full value for this key
     value=$(read_value "$key" < .env)
     
+    # Remove quotes if present
+    value=${value#\"}
+    value=${value%\"}
+    value=${value#\'}
+    value=${value%\'}
+    
     # Map .env keys to values.yaml paths
     case "$key" in
-        "AWS_REGION")
-            yq -i ".aws.region = \"$value\"" values.yaml
+        "GUID")
+            yq -i ".global.guid = \"$value\"" values.yaml
             ;;
-        "AWS_ACCESS_KEY")
+        "AWS_ACCESS_KEY_ID")
             yq -i ".aws.credentials.accessKey = \"$value\"" values.yaml
             ;;
-        "AWS_SECRET_KEY")
+        "AWS_SECRET_ACCESS_KEY")
             yq -i ".aws.credentials.secretKey = \"$value\"" values.yaml
             ;;
         "AWS_BASE_DOMAIN")
@@ -66,16 +77,12 @@ while IFS='=' read -r line; do
             yq -i ".cluster.namespace = \"$value\"" values.yaml
             ;;
         "PULL_SECRET")
-            # Remove surrounding quotes if present
-            value=${value#\"}
-            value=${value%\"}
-            value=${value#\'}
-            value=${value%\'}
+            # Handle JSON pull secret
             echo "$value" | yq -i '.auth.pullSecret = load("/dev/stdin")' values.yaml
             ;;
         "SSH_PRIVATE_KEY")
-            # Preserve newlines for SSH keys
-            yq -i ".auth.sshPrivateKey = \"$value\"" values.yaml
+            # Handle base64 encoded SSH private key
+            echo "$value" | base64 -d | yq -i '.auth.sshPrivateKey = load("/dev/stdin")' values.yaml
             ;;
         "SSH_PUBLIC_KEY")
             yq -i ".auth.sshPublicKey = \"$value\"" values.yaml
